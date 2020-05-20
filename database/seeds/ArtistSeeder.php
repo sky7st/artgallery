22529785e4855ac3302a6ae04a43c6a58663d23f
sky7st <s1k2y37st@gmail.com>
2020-05-20 17:40:29 +0800

    add artist seeder

---
+++ database/seeds/ArtistSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Artist;
+
+class ArtistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $artists = [];
+        $f = fopen( base_path()."/database/seeds/artist_parsed.csv", "r");
+        while (($data = fgetcsv($f, 1000, ",")) !== FALSE) 
+        {
+            $artists[] = $data;
+        }
+        fclose($f);
+        foreach($artists as $i=>$row){
+            if($i !== 0){
+                $artist = new Artist;
+                $artist->artist_ssn = $row[0];
+                $artist->name = $row[1];
+                $artist->address = $row[2];
+                $artist->phone = $row[3];
+                $artist->usual_type = $row[4];
+                $artist->usual_medium = $row[5];
+                $artist->usual_style = $row[6];
+                $artist->sales_last_year = (int)$row[7];
+                $artist->sales_year_to_date = (int)$row[8];
+                $artist->save();
+            } 
+        }
+    }
+}
