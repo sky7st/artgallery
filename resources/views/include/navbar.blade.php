b4214e342053238597f43cfd083409458f825db4
neil <s1k2y37st@gmail.com>
2020-05-22 17:57:28 +0800

    fix navbar

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -24,9 +24,9 @@
                     <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                     <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                 @else
-                    <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+                    {{-- <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
                     <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
-                    <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li>
+                    <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li> --}}
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
